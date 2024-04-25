6190b8fa9a87b8ca25cb3c1bcd9cba3567f9f595
Manuel Alejandro Mañodes Cofré <manuelalejandromanodescofre@iMac-de-Manuel.local>
2024-04-24 22:31:26 -0400

    PREENTREGA1MANODES: Segunda versión con ideas mejoradas en el manejo de las respuestas del usuario

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-// Preguntar al usuario por su nombre
-var name = prompt("Cuál es tu nombre?");
-
-// Preguntar al usuario por su edad
-var age = prompt("Cuál es tu edad?");
-
-// Preguntar al usuario por su ocupación
-var occupation = prompt("Cuál es tu ocupación?");
-
-// Preguntar al usuario por su preferencia entre programación y diseño
-var preference = prompt("Prefieres programación o diseño?");
+// Preguntas a usuario
+let nombre = prompt("¿Cuál es tu nombre?");
+let edad = prompt("¿Cuál es tu edad?");
+let ocupacion = prompt("Por último ¿A qué te dedicas?");
+let preferencia = prompt("¿Cuál es tu preferencia? Selecciona una opción:\n1. Programación\n2. Diseño\n3. Otro");
 
 // Verificar la respuesta del usuario y mostrar un mensaje de acuerdo a su preferencia
-if (preference === "programación") {
-  alert("Hola " + name + ", tu respuesta indica que prefieres programación. ¡Eres un futuro programador!");
-} else if (preference === "diseño") {
-  alert("Hola " + name + ", tu respuesta indica que prefieres diseño. ¡Eres un futuro diseñador!");
+if (preferencia == "1") {
+  alert("Has seleccionado programación.");
+} else if (preferencia == "2") {
+  alert("Has seleccionado diseño.");
+} else if (preferencia == "3") {
+  alert("Has seleccionado otra opción.");
 } else {
-  alert("Hola " + name + ", tu respuesta no fue clara. Por favor intenta de nuevo.");
+  alert("Selección no válida.");
 }
