9ea17afbb883dcb2add69ada7d9f6e4c410b5fb3
Manuel Alejandro Mañodes Cofré <manuelalejandromanodescofre@iMac-de-Manuel.local>
2024-04-29 14:16:49 -0400

    PREENTREGA1MANODES: Versión final con flujo para entrega

--- main.js
+++ main.js
@@ -1,16 +1,66 @@
-// Preguntas a usuario
-let nombre = prompt("¿Cuál es tu nombre?");
-let edad = prompt("¿Cuál es tu edad?");
-let ocupacion = prompt("Por último ¿A qué te dedicas?");
-let preferencia = prompt("¿Cuál es tu preferencia? Selecciona una opción:\n1. Programación\n2. Diseño\n3. Otro");
 
-// Verificar la respuesta del usuario y mostrar un mensaje de acuerdo a su preferencia
-if (preferencia == "1") {
-  alert("Has seleccionado programación.");
-} else if (preferencia == "2") {
-  alert("Has seleccionado diseño.");
-} else if (preferencia == "3") {
-  alert("Has seleccionado otra opción.");
-} else {
-  alert("Selección no válida.");
-}
+// En primera instancia creo una función que encapsule todo el script
+function obtenerDatosUsuario() {
+
+  // Creo una alert para contextualizar y dar la bienvenida al usuario
+  alert("¡Bienvenido al Test de preferencias!\nContesta las siguientes preguntas para conocer tu vocación.");
+
+      // Luego, creo variables que obtendrán información de usuario    
+      let nombre = prompt("Para empezar nuestra conversación, ¿podrías decirme tu nombre?");
+      let edad = parseInt(prompt("¿Cuántos años tienes?"));
+      let ocupacion = prompt("Por último, ¿Qué es lo que haces en tu día a día?");
+      
+      // A continuación defino un arreglo el cual muestra diferentes preguntas y orienta
+      // al usuario para que su respuesta sea un valor entre 1 a 5
+      const preguntas = [
+          { pregunta: "En una escala del 1 al 5, ¿qué tan cómodo te sientes programando? (1: No cómodo en absoluto, 5: Muy cómodo)" },
+          { pregunta: "¿Qué tan hábil te consideras en diseño visual? (1: No hábil en absoluto, 5: Muy hábil)" },
+          { pregunta: "En una escala del 1 al 5, ¿qué tan hábil te consideras en la gestión de proyectos? (1: Poco hábil, 5: Muy hábil)" },
+          { pregunta: "En una escala del 1 al 5, ¿qué tan interesado estás en el análisis de datos? (1: Nada interesado, 5: Muy interesado)" }
+
+      ];
+      
+      // Defino una variable en 0 para alojar la suma de los valores
+      let sumaValoraciones = 0;
+  
+      // Acá genero un bucle para asegurar que la respuesta del arreglo de preguntas sea un 
+      // valor númerico
+      for (let i = 0; i < preguntas.length; i++) {
+          let respuesta;
+          do {
+              respuesta = parseInt(prompt(preguntas[i].pregunta));
+              if (isNaN(respuesta)) {
+                  alert("Por favor, introduce un valor numérico válido.");
+              }
+          } while (isNaN(respuesta));
+          
+          // Sumo la variable en 0 + la suma de las respuestas del arreglo.
+          sumaValoraciones += respuesta;
+      }
+
+      // En este bloque define según condiciones el valor de preferencias, según
+      // las respuestas del usuario 
+      let preferencia;
+      if (sumaValoraciones >= 8) {
+          preferencia = "Programación";
+      } else if (sumaValoraciones >= 5) {
+          preferencia = "Diseño";
+      } else {
+          preferencia = "Otro";
+      }
+  
+      // Se genera una respuesta para el usuario
+      alert("Basado en tus respuestas, tu preferencia es: " + preferencia);
+  
+      // Genero una variable y una sentencia para confirmar si el usuario quiere repetir
+      // el proceso o simplemente cerrar.
+      let volverATestear = confirm("¿Quieres realizar el test nuevamente?");
+      if (volverATestear) {
+          obtenerDatosUsuario();
+      } else {
+          alert("Gracias por participar en el test. ¡Hasta luego!");
+      }
+  }
+  
+  // Se ejecuta la función
+  obtenerDatosUsuario();
